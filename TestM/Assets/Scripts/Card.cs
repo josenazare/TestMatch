b98987b4eb2387afab5c5b4f2ecb67f25173ece8
dev-josenazare <jose@vincellstudios.com>
2024-12-01 15:42:51 +0530

    Dynamic Matrix generation
    Generates dynamic matrix based on number of rows and columns

---
+++ TestM/Assets/Scripts/Card.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.UI; // Required for UI elements
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card : MonoBehaviour,IPointerDownHandler
+{
+    public CardSO CardSOData; // ScriptableObject reference
+    public bool isFlipped = false; // Whether the card is flipped
+    public Sprite CardImage; // Front side image of the card
+
+    Quaternion initialQuart = new Quaternion();
+    Quaternion FinalQuart = new Quaternion();
+
+
+    // Reference to the UI Image to display the card
+
+    private void Start()
+    {
+        if (CardSOData != null)
+        {
+            // Assign the sprite from the ScriptableObject to the card
+            CardImage = CardSOData.Image;
+
+            GetComponent<Image>().sprite = CardImage;
+        }
+    }
+
+ public   IEnumerator RotateCard(GameObject Card,bool FlipBack = true)
+    {
+        GameManager.instance.eventSystem.gameObject.SetActive(false);
+        initialQuart = Card.transform.rotation;
+        FinalQuart = Card.transform.rotation * Quaternion.Euler(0f, 180f, 0f);
+        float elapsedTime = 0f;
+
+        // Smoothly interpolate over time
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime;
+
+
+              Card.transform.rotation = Quaternion.Slerp(initialQuart,FinalQuart,t);
+            if (elapsedTime > 0.5f)
+            {
+                if (FlipBack)
+                {
+                    Card.GetComponent<Image>().sprite = CardImage;
+                }
+                else
+                {
+                    Card.GetComponent<Image>().sprite = UIManager.instance.CardBackImage;
+                }
+            }
+
+            yield return null; // Wait for the next frame
+        }
+        GameManager.instance.eventSystem.gameObject.SetActive(true);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       GameManager.instance.eventSystem.gameObject.SetActive(false);
+        //  GameManager.instance.eventSystem.enabled = false;
+        isFlipped = true;
+        if (GameManager.instance.CardInBuffer == null)
+        {
+            GameManager.instance.CardInBuffer = this;
+
+            GameManager.instance.CardInBuffer.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            StartCoroutine(RotateCard(GameManager.instance.CardInBuffer.gameObject));
+        }
+        else
+        {
+            GameManager.instance.CurrentlySelected = this;
+            GameManager.instance.CurrentlySelected.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            StartCoroutine(RotateCard(GameManager.instance.CurrentlySelected.gameObject));
+            GameManager.instance.CheckPair();
+        }
+    }
+}
+
---
+++ TestM/Assets/Scripts/UIManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GridLayoutGroup gridLayout;
+    public RectTransform gridLayoutTransform;
+    public GameObject card, EmptyGameObject;
+    public int rowSize, columnSize;
+    bool isEven;
+    public Sprite CardBackImage;
+    public List<Sprite> CardImages;
+
+    public static UIManager instance;
+    int midRow;
+    int midColumn;
+   
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+        rowSize = Mathf.Clamp(rowSize, 2, 6);
+        columnSize = Mathf.Clamp(columnSize, 2, 6);
+
+        if (rowSize * columnSize % 2 == 0)
+        {
+            isEven = true;
+        }
+
+        if (!isEven)
+        {
+            midRow = rowSize / 2;
+            midColumn = columnSize / 2;
+
+        }
+      GameManager.instance.NumberOfPossiblePairs = rowSize * columnSize / 2;
+
+        GenerateCards();
+
+    }
+
+    public void GenerateCards()
+    {
+        
+        
+        // Get total container dimensions
+        float totalAvailableWidth = gridLayoutTransform.rect.width;
+        float totalAvailableHeight = gridLayoutTransform.rect.height;
+
+        //  dynamic spacing
+        float spacingX = totalAvailableWidth * 0.02f; // 2% of container width
+        float spacingY = totalAvailableHeight * 0.02f; // 2% of container height
+
+        // Calculate card size by subtracting total spacing
+        float cardWidth = (totalAvailableWidth - (spacingX * (columnSize - 1))) / columnSize;
+        float cardHeight = (totalAvailableHeight - (spacingY * (rowSize - 1))) / rowSize;
+
+        // Update GridLayout settings
+        gridLayout.cellSize = new Vector2(cardWidth, cardHeight);
+        gridLayout.spacing = new Vector2(spacingX, spacingY);
+
+
+        int Imgcount = 0;
+        // Instantiate cards dynamically
+        for (int i = 0; i < rowSize ; i++)
+        {
+            for (int j = 0; j < columnSize ; j++)
+            {
+                // Instantiate the card prefab
+                
+
+                if (!isEven)
+                {
+                    if (i == midRow && j == midColumn)
+                    {
+                        Instantiate(EmptyGameObject, gridLayout.transform);
+                        continue;
+                    }
+                }
+
+                GameObject cardClone = Instantiate(card, gridLayout.transform);
+
+                // Get the Card component and assign the ScriptableObject
+                Card cardComponent = cardClone.GetComponent<Card>();
+
+                if (cardComponent != null)
+                {
+                    if (Imgcount < GameManager.instance.NumberOfPossiblePairs)
+                    {
+                        cardComponent.CardSOData = GameManager.instance.AllCardsData[Imgcount];
+                       
+                    }
+                    else
+                    {
+                        Imgcount = 0;
+                        cardComponent.CardSOData = GameManager.instance.AllCardsData[Imgcount];
+                    }
+                    Imgcount++;
+                }
+            }
+
+        }
+        StartCoroutine(GameManager.instance.InitialFlipCards());
+    }
+}
+
