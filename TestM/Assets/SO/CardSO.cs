8ad98cd6f8d18f59ee499ab338e4cb49076c418b
dev-josenazare <jose@vincellstudios.com>
2024-12-01 15:43:52 +0530

    Scriptable objects
    Scriptable object data for each card

---
+++ TestM/Assets/SO/CardSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject")]
+public class CardSO : ScriptableObject
+{
+    public int ID;
+    public Sprite Image;
+   // public bool isFlipped;
+}
